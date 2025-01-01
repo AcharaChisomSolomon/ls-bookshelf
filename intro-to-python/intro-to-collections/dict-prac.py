44cddf818618400b635ca9a82a50db27bfcfcdf5
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-01 09:10:13 +0000

    fun intro to collections

---
+++ intro-to-python/intro-to-collections/dict-prac.py
@@ -0,0 +1,11 @@
+country_mapper = {
+    "Alice": "USA",
+    "Francois": "canada",
+    "Inti": "Peru",
+    "Monika": "Germany",
+    "Sanyu": "Uganda",
+    "Yoshitaka": "Japan"
+}
+
+print(country_mapper["Alice"])
+print(country_mapper["Sanyu"])
