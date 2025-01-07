232b846fe782ecf41bb7381feedbc983eaafeb95
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-07 20:14:19 +0000

    ex. 7 done

--- intro-to-python/using-collections/exercises.py
+++ intro-to-python/using-collections/exercises.py
@@ -1,10 +1,12 @@
 # 1
 # print(range(0, 25, 3)[6])
 
+
 # 2
 # string = "Launch School"
 # id = string.find("c")
 # print(string[id:id + 6])
+
 
 # 3
 # orig_tuple = (1, 2, 3, 4, 5)
@@ -15,12 +17,19 @@
 # new_tuple = tuple(list_from_tuple)
 # print(new_tuple)
 
+
 # 4
-pets = {
-    'Cat':  'Meow',
-    'Dog':  'Bark',
-    'Bird': 'Tweet',
-}
-print(pets['Dog'])
-print(pets.get("Lizard"))
-print(pets.get("Lizard", "<silence>"))
+# pets = {
+#     'Cat':  'Meow',
+#     'Dog':  'Bark',
+#     'Bird': 'Tweet',
+# }
+# print(pets['Dog'])
+# print(pets.get("Lizard"))
+# print(pets.get("Lizard", "<silence>"))
+
+
+# 7
+info = 'xyz:*:42:42:Lee Kim:/home/xyz:/bin/zsh'
+new_info = "+".join(info.split(":"))
+print(new_info)
